dae7a720f32603cb09935add702859737ec23e94
Robert Guss <robertguss@gmail.com>
2016-08-30 16:53:51 -0400

    added font-awesome and the ability to add todos, toggle completed todos, and remove todos

--- todo-app/src/js/app.js
+++ todo-app/src/js/app.js
@@ -1,1 +1,33 @@
-console.log('working...');
+new Vue({
+  el: '#app',
+  template: '#todoTemplate',
+
+  data: {
+
+    newTodo: '',
+
+    todoList: [
+      {task: 'Go grocery shopping', completed: false},
+      {task: 'Pick up kids from school', completed: false},
+      {task: 'Pay phone bill', completed: false}
+    ]
+  },
+
+  methods: {
+    addTodo: function() {
+      var todoTask = this.newTodo.trim();
+      if (todoTask) {
+        this.todoList.push({task: todoTask, complete: false});
+        this.newTodo = '';
+      }
+    },
+
+    removeTodo: function(index) {
+      this.todoList.splice(index, 1);
+    },
+
+    completeTodo: function(index) {
+      this.todoList[index].completed = !this.todoList[index].completed;
+    }
+  }
+});
