a257774c22c491be07cd381fedb00bf2acdfa470
Robert Guss <robertguss@gmail.com>
2016-08-31 17:00:06 -0400

    made a successful api call to openweathermap.org v-for loop is broken though

--- weather-app/src/js/app.js
+++ weather-app/src/js/app.js
@@ -1,9 +1,23 @@
+const apiKey = '5c9da600a7c42093f56acd5ea7466037';
+const apiURI = `http://api.openweathermap.org/data/2.5/weather?q=boston,ma&units=imperial&APPID=${apiKey}`
+
+
+
 new Vue({
   el: '#app',
   template: '#weatherTemplate',
 
   data: {
+    weatherData: []
+  },
 
+  ready: function() {
+    this.$http.get(apiURI).then((response) => {
+      this.$set('weatherData', response.json())
+      console.log(response);
+      }, (response) => {
+        console.log('There was an error...');
+    });
   },
 
   methods: {
