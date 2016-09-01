a1e12a2c22e3a1dc00ec53869706ccee94db8ef5
Robert Guss <robertguss@gmail.com>
2016-09-01 11:00:30 -0400

    added brew-app scaffold

---
+++ brew-app/src/js/app.js
@@ -0,0 +1,23 @@
+// This turns off the following errors in the JS console.
+// [Vue warn]: Error when evaluating expression "weatherData.main.temp": TypeError: Cannot read property 'temp' of undefined
+Vue.config.warnExpressionErrors = false
+
+
+
+new Vue({
+  el: '#app',
+  template: '#brewTemplate',
+
+  data: {
+
+  },
+
+  
+  ready: function() {
+
+  },
+
+  methods: {
+
+  }
+});
