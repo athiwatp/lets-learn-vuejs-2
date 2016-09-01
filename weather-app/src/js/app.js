4ce52c0ed22f4f73b558f37e563b6168f937608b
Robert Guss <robertguss@gmail.com>
2016-08-31 23:03:44 -0400

    added current weather and forecast weather and bootstrap grid css

--- weather-app/src/js/app.js
+++ weather-app/src/js/app.js
@@ -1,5 +1,5 @@
-const apiKey = '5c9da600a7c42093f56acd5ea7466037';
-const apiURI = `http://api.openweathermap.org/data/2.5/weather?q=boston,ma&units=imperial&APPID=${apiKey}`
+// const apiKey = '5c9da600a7c42093f56acd5ea7466037';
+// const apiURI = `http://api.openweathermap.org/data/2.5/weather?q=boston,ma&units=imperial&APPID=${apiKey}`
 
 
 
@@ -8,19 +8,50 @@ new Vue({
   template: '#weatherTemplate',
 
   data: {
-    weatherData: []
+    city: 'Providence',
+    state: 'RI',
+    weatherLocation: {city: '', state: ''},
+    weatherData: [],
+    weatherForecast: [],
   },
 
   ready: function() {
+    const apiURI = `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&units=imperial&APPID=5c9da600a7c42093f56acd5ea7466037`
     this.$http.get(apiURI).then((response) => {
       this.$set('weatherData', response.json())
-      // console.log(this.weatherData);
+      this.weatherLocation.city = this.city;
+      this.weatherLocation.state = this.state;
       }, (response) => {
         console.log('There was an error...');
     });
+
   },
 
   methods: {
+    getWeather: function() {
+      const apiURI = `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&units=imperial&APPID=5c9da600a7c42093f56acd5ea7466037`
+      this.$http.get(apiURI).then((response) => {
+        this.$set('weatherData', response.json())
+        this.weatherLocation.city = this.city;
+        this.weatherLocation.state = this.state;
+        this.city = '';
+        this.state = '';
+        }, (response) => {
+          console.log('There was an error...');
+      });
+    },
 
+    getForecast: function() {
+      const apiURI = `http://api.openweathermap.org/data/2.5/forecast?q=${this.city},${this.state}&units=imperial&APPID=5c9da600a7c42093f56acd5ea7466037`
+      this.$http.get(apiURI).then((response) => {
+        this.$set('weatherForecast', response.json())
+          this.weatherLocation.city = this.city;
+          this.weatherLocation.state = this.state;
+          this.city = '';
+          this.state = '';
+        }, (response) => {
+          console.log('There was an error...');
+      });
+    }
   }
 });
